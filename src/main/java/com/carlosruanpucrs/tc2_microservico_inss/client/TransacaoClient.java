e84546fec38a6130b408ada65ee482e39a589128
Carlos Ruan <c.ruan@edu.pucrs.br>
2025-11-23 17:21:26 -0300

    refactor

--- src/main/java/com/carlosruanpucrs/tc2_microservico_inss/client/TransacaoClient.java
+++ src/main/java/com/carlosruanpucrs/tc2_microservico_inss/client/TransacaoClient.java
@@ -6,12 +6,13 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Component
 @FeignClient(name = "transacao-service", path = "/v1/pagamentos/inss")
 public interface TransacaoClient {
 
-    @GetMapping("/{numeroConta}")
+    @PostMapping
     ResponseEntity<PagamentoInssResponse> pagarInss(@RequestBody PagamentoInssRequest request);
 }
